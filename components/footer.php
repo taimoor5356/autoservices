7a4f1ee4ce39e9f42753b96e0eb60c54996e7704
taimoor5356 <mtaimoornaveed@gmail.com>
2025-07-05 12:03:26 +0500

    updated

--- components/footer.php
+++ components/footer.php
@@ -71,7 +71,7 @@
                                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                                     Copyright &copy;<script>
                                         document.write(new Date().getFullYear());
-                                    </script> All rights reserved by <a href="https://navsons.pk" target="_blank">Navsons Group</a>
+                                    </script> All rights reserved by <a href="https://navsons.com" target="_blank">Navsons Group</a>
                                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                             </div>
                         </div>
